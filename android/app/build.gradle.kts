92eff76c1df14530080ea4b2b8117cc4e341a7d8
shishukagoz <shishukagoz@gmail.com>
2025-03-05 07:58:27 +0000

    Fixed the issues

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -7,9 +7,10 @@ plugins {
 
 android {
     namespace = "com.shihanrishad.apkjoy"
-    compileSdk = flutter.compileSdkVersion
+  //  compileSdk = flutter.compileSdkVersion
+    compileSdk = 34
     // ndkVersion = flutter.ndkVersion
-    ndkVersion = "27.0.12077973"
+    ndkVersion = "27.2.12479018"
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_1_8
@@ -22,11 +23,13 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.myapp"
+        applicationId = "com.shihanrishad.apkjoy"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
-        minSdk = flutter.minSdkVersion
-        targetSdk = flutter.targetSdkVersion
+     //   minSdk = flutter.minSdkVersion
+        minSdk = 21
+     //   targetSdk = flutter.targetSdkVersion
+        targetSdk = 34
         versionCode = flutter.versionCode
         versionName = flutter.versionName
     }
